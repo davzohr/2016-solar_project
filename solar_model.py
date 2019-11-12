519ddabae12fdbdeb7e181849b36715866286f78
ssyrovatkin <Stepashan@bk.ru>
2019-11-12 14:47:07 +0300

    add formula for forces and coords

--- solar_model.py
+++ solar_model.py
@@ -2,6 +2,7 @@
 # license: GPLv3
 
 gravitational_constant = 6.67408E-11
+G =gravitational_constant
 """Гравитационная постоянная Ньютона G"""
 
 
@@ -19,8 +20,8 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        body.Fx += G*body.m*obj.m/(r**3)*(body.x - obj.x) # FIXME: нужно вывести формулу...
+        body.Fy += G*body.m*obj.m/(r**3)*(body.y - obj.y)  # FIXME: нужно вывести формулу...
 
 
 def move_space_object(body, dt):
@@ -32,9 +33,12 @@ def move_space_object(body, dt):
     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
+    ay = body.Fy/body.m
     body.Vx += ax*dt
-    # FIXME: not done recalculation of y coordinate!
+    body.x += body.Vx*dt
+    body.Vy += ay*dt
+    body.y += body.Vy*dt
+    return
 
 
 def recalculate_space_objects_positions(space_objects, dt):
